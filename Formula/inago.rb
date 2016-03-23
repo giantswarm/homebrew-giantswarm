d7a93356df7dd09ca4f2563cbcce1d981e6fe0ad
Nesurion <maik.schwan@gmail.com>
2016-03-23 14:47:59 +0100

    adds inago formula

---
+++ Formula/inago.rb
@@ -0,0 +1,11 @@
+class Inago < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/giantswarm/inago/releases/download/0.1.0/inago.0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "c271324495e3bdb9c77f404c3cca084300c96b3fe6f28f6882ededcc85fc6539"
+  
+  def install
+      bin.install Dir["darwin/*"]
+  end
+end

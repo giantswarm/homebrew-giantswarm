54a30b3e04603d4b4f980669b80c820e4f493b10
Nesurion <maik.schwan@gmail.com>
2016-03-23 14:55:38 +0100

    rename inago to inagoctl

--- Formula/inagoctl.rb
+++ Formula/inagoctl.rb
@@ -1,4 +1,4 @@
-class Inago < Formula
+class Inagoctl < Formula
   desc "Inago orchestrates groups of units on fleet clusters"
   homepage "https://github.com/giantswarm/inago"
   url "https://github.com/giantswarm/inago/releases/download/0.1.0/inago.0.1.0.tar.gz"

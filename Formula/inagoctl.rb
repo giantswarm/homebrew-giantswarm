4a1483955cc5b8ae3858f442ee6461dd29c24acd
Nesurion <maik.schwan@gmail.com>
2016-03-23 15:02:08 +0100

    update sha

--- Formula/inagoctl.rb
+++ Formula/inagoctl.rb
@@ -3,7 +3,7 @@ class Inagoctl < Formula
   homepage "https://github.com/giantswarm/inago"
   url "https://github.com/giantswarm/inago/releases/download/0.1.0/inago.0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "c271324495e3bdb9c77f404c3cca084300c96b3fe6f28f6882ededcc85fc6539"
+  sha256 "a9411f751ccbe61db57a1c26a0742c7157e8ad7145137d63db26b44a13bf517d"
   
   def install
       bin.install Dir["darwin/*"]

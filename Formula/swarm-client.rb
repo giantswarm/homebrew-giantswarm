f6a9e2537d6706dbe81911dc0349e044f86801f7
Nesurion <maik.schwan@gmail.com>
2016-04-12 09:19:25 +0200

    use sha256 hash for current swarm-client release

--- Formula/swarm-client.rb
+++ Formula/swarm-client.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class SwarmClient < Formula
   homepage "https://giantswarm.io"
-  # openssl dgst -sha1 <file>
+  # openssl dgst -sha256 <file>
   url "https://downloads.giantswarm.io/swarm/clients/0.27.0/swarm-0.27.0-darwin-amd64.tar.gz"
-  sha1 "7432927c2609cc9edeac912123b468f9d055cbf1"
+  sha256 "3defc4bac2f7e64b299b87a7a7199e855f65ce0f1e2943f390e047310ade9c47"
   version "0.27.0"
 
   def install

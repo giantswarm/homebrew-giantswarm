43fa501a446a9b8d8d92903ebd8665cb6821066b
Nesurion <maik.schwan@gmail.com>
2016-04-19 11:06:00 +0200

    bump version to 0.2.2

--- Formula/inagoctl.rb
+++ Formula/inagoctl.rb
@@ -1,9 +1,10 @@
 class Inagoctl < Formula
   desc "Inago orchestrates groups of units on fleet clusters"
   homepage "https://github.com/giantswarm/inago"
-  url "https://github.com/giantswarm/inago/releases/download/0.2.1/inagoctl.0.2.1.darwin.tar.gz"
-  version "0.2.1"
-  sha256 "e1a6ecd639064ac779c3e6e3d3e8e3f9c242d0ceea919418a409a706e1e89350"
+  url "https://github.com/giantswarm/inago/releases/download/0.2.2/inagoctl.0.2.2.darwin.tar.gz"
+  version "0.2.2"
+  # openssl dgst -sha256 <file>
+  sha256 "a4e062d3ce02540c1661dcd90902950de06b4058f49b3ba858e04407658af9cf"
   
   def install
       bin.install Dir["*"]

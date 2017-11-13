ebed9cb0d70a4011633ac35c1a4b564dcb780935
Marian Steinbach <marian@giantswarm.io>
2017-11-13 09:50:08 +0100

    Updated gsctl to 0.8.4

--- Formula/gsctl.rb
+++ Formula/gsctl.rb
@@ -6,10 +6,10 @@ require "formula"
 class Gsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/gsctl"
-  url "http://downloads.giantswarm.io/gsctl/0.8.3/gsctl-0.8.3-darwin-amd64.tar.gz"
-  version "0.8.3"
+  url "http://downloads.giantswarm.io/gsctl/0.8.4/gsctl-0.8.4-darwin-amd64.tar.gz"
+  version "0.8.4"
   # openssl dgst -sha256 <file>
-  sha256 "3c796403dfd0b038afecfeae78d73cf0a96fdd5f87c5e451cc2d9d0a9793f38a"
+  sha256 "969a98e573a40074ca5cafdca7ac739393c467bd55ad25db69ef29c027715c45"
 
   def install
     bin.install "gsctl"

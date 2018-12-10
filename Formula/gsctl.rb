8c135baf178507fb343e6806cb34f3105486874e
taylorbot <dev@giantswarm.io>
2018-12-10 10:40:58 +0000

    Update gsctl to 0.14.4

--- Formula/gsctl.rb
+++ Formula/gsctl.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 # This file is generated automatically by
-# https://github.com/giantswarm/gsctl/blob/master/update-homebrew.sh
+# https://github.com/giantswarm/gsctl/blob/master/.circleci/update-homebrew.sh
 
 class Gsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/gsctl"
-  url "http://downloads.giantswarm.io/gsctl/0.14.3/gsctl-0.14.3-darwin-amd64.tar.gz"
-  version "0.14.3"
+  url "http://downloads.giantswarm.io/gsctl/0.14.4/gsctl-0.14.4-darwin-amd64.tar.gz"
+  version "0.14.4"
   # openssl dgst -sha256 <file>
-  sha256 "8ab968a5eab95d714bca85b7db71be782a863c2620f5f7a109283d0976d0ebc0"
+  sha256 "63b2ae80c09d97eb19f04eb7afdb1bdd54fed3d49a3039d052bf5aa0258339fd"
 
   def install
     bin.install "gsctl"

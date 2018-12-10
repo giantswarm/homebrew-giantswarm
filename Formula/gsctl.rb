31d574573809de81d8f0da6b4ad6ea3e89fd34f9
Marian Steinbach <marian@giantswarm.io>
2018-12-10 15:29:09 +0100

    Change download URL

--- Formula/gsctl.rb
+++ Formula/gsctl.rb
@@ -6,7 +6,7 @@ require "formula"
 class Gsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/gsctl"
-  url "http://downloads.giantswarm.io/gsctl/0.14.4/gsctl-0.14.4-darwin-amd64.tar.gz"
+  url "https://github.com/giantswarm/gsctl/releases/download/0.14.4/gsctl-0.14.4-darwin-amd64.tar.gz"
   version "0.14.4"
   # openssl dgst -sha256 <file>
   sha256 "63b2ae80c09d97eb19f04eb7afdb1bdd54fed3d49a3039d052bf5aa0258339fd"

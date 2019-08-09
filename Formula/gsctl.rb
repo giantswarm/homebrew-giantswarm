387d4af422f88c5b3c831f01725598d35a6b781e
Marian Steinbach <marian@giantswarm.io>
2019-08-09 11:11:25 +0200

    Attempt to make gsctl formula cross-platform

--- Formula/gsctl.rb
+++ Formula/gsctl.rb
@@ -6,10 +6,32 @@ require "formula"
 class Gsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/gsctl"
-  url "https://github.com/giantswarm/gsctl/releases/download/0.15.4/gsctl-0.15.4-darwin-amd64.tar.gz"
   version "0.15.4"
+
+  def os
+    case RUBY_PLATFORM
+    when /darwin/
+      :darwin
+    when /linux/
+      :linux
+    else
+      :unknown
+  end
+
+  def url
+    if os == :darwin
+      "https://github.com/giantswarm/gsctl/releases/download/0.15.4/gsctl-0.15.4-darwin-amd64.tar.gz"
+    elsif os == :linux
+      "https://github.com/giantswarm/gsctl/releases/download/0.15.4/gsctl-0.15.4-linux-amd64.tar.gz"
+  end
+
   # openssl dgst -sha256 <file>
-  sha256 "fde56eb46c5cdcf256f7aaffa0e83cfe7b954308abba37f494c40452c01280ef"
+  def
+    if os == :darwin
+      "fde56eb46c5cdcf256f7aaffa0e83cfe7b954308abba37f494c40452c01280ef"
+    elsif os == :linux
+      "703abb2a55f9861becd5442961413edeb95fc3185b8056333f72b688e5eb6741"
+  end
 
   def install
     bin.install "gsctl"

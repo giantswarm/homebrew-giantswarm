d03097658ee51515a96cc4364737345635e50d05
taylorbot <dev@giantswarm.io>
2019-09-04 09:50:04 +0000

    Update gsctl to 0.16.0

--- Formula/gsctl.rb
+++ Formula/gsctl.rb
@@ -6,10 +6,10 @@ require "formula"
 class Gsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/gsctl"
-  url "https://github.com/giantswarm/gsctl/releases/download/0.15.4/gsctl-0.15.4-darwin-amd64.tar.gz"
-  version "0.15.4"
+  url "https://github.com/giantswarm/gsctl/releases/download/0.16.0/gsctl-0.16.0-darwin-amd64.tar.gz"
+  version "0.16.0"
   # openssl dgst -sha256 <file>
-  sha256 "fde56eb46c5cdcf256f7aaffa0e83cfe7b954308abba37f494c40452c01280ef"
+  sha256 "7c8e184dc0f191d66e5ba0f27db3e25e0901aff7cc8424027b6f32a654ed60af"
 
   def install
     bin.install "gsctl"
